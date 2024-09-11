38e078700985067ec5807588c7b3b167de6f8928
Marcorr03 <marcoanrr0314@gmail.com>
2024-09-11 11:01:11 -0600

    Update calculate_Area.js

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -16,7 +16,7 @@ function calculateTotal(){
     grocery1=parseFloat(document.getElementById('grocery1').value);
     grocery2=parseFloat(document.getElementById('grocery2').value);
     grocery3=parseFloat(document.getElementById('grocery3').value);
-    let total = grocery1 +grocery2 + grocery3;
+    let total = grocery1 + grocery2 + grocery3;
     document.getElementById('total').innerText = `The total amount is:$ ${total}`;
 }
  
